3d9503345acd7f9e8d62ae55097d2501c8af3e3b
SDBagel <sdbagel@gmail.com>
2022-02-11 23:08:36 -0800

    refactor: Pull test badge from dev API

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 
-import { BadgeService } from "./badge/badge.ts";
+import { generate } from "./badge/badge.ts";
 import { Application, Router } from "./deps.ts";
 
 const app = new Application();
@@ -7,16 +7,10 @@ const router = new Router();
 
 router
   .get("/test", async ctx => {
-    const badge = {
-      "id":1,
-      "fields":[
-        {"content":"New","color":0,"width":35},
-        {"content":"Badge","color":4,"width":50}
-      ],
-      "style":2
-    }
+    const badge = await fetch("http://localhost:8002/SDBagel/Analytics/1/json")
+      .then(res => res.json());
 
-    ctx.response.body = await new BadgeService().generate(badge);
+    ctx.response.body = await generate(badge);
     ctx.response.type = "image/svg+xml";
   });
 
