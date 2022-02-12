3abf9612ab7457f65168ed6b6d549994abc037d8
SDBagel <sdbagel@gmail.com>
2022-02-11 23:08:17 -0800

    refactor: Export functions directly

--- badge/badge.ts
+++ badge/badge.ts
@@ -3,7 +3,7 @@ import { mapColor } from "./color.ts";
 import { generateField } from "./field.ts";
 import { mapStyle } from "./style.ts";
 import { generateWrapper } from "./wrapper.ts";
-import { IconService } from "../icon/icon.ts";
+import { getIconDataURL } from "../icon/icon.ts";
 import { Badge, BadgeStyle, BadgeField } from "../schema/mod.ts";
 
 interface BadgePartial {
@@ -12,75 +12,65 @@ interface BadgePartial {
 	width: number // Width calculation (SVG)
 }
 
-export class BadgeService {
-	private iconService: IconService;
-	constructor() {
-		this.iconService = new IconService();
-	}
-
-	async generate(badge: Badge): Promise<string> {
-		let totalWidth = 0;
-		let innerContent = "";
-		let accessibleTitle: string[] = [];
-		
-		// Compile badge partials
-		if (badge.fields)
-			for await (const field of badge.fields) {
-				const part = await this.generatePartial(field, badge.style, totalWidth);
-				totalWidth += part.width;
-				innerContent += part.content;
-				accessibleTitle.push(part.title);
-			}
-
-		const aTitle = accessibleTitle.join(" ");
-		const mappedStyle = mapStyle(badge.style);
-		return generateWrapper({
-			content: innerContent,
-			title: aTitle,
-			height: mappedStyle.height,
-			width: totalWidth,
-		});
-	}
-
-	private async generatePartial(field: BadgeField, style: BadgeStyle, offset = 0): Promise<BadgePartial> {
-		// Parse potential icon (:iconqualifier:)
-		if (field.iconURI) {
-			field.content = field.content.replace(/^:(.+):/, "").trim();
-			field.iconURI = await this.iconService.getIconDataURL(field.iconURI, true);
+export async function generate(badge: Badge): Promise<string> {
+	let totalWidth = 0;
+	let innerContent = "";
+	let accessibleTitle: string[] = [];
+	
+	// Compile badge partials
+	if (badge.fields)
+		for await (const field of badge.fields) {
+			const part = await generatePartial(field, badge.style, totalWidth);
+			totalWidth += part.width;
+			innerContent += part.content;
+			accessibleTitle.push(part.title);
 		}
 
-		// If Dynamic, get the DVS content
-		if (field.source) {
-			await fetch(field.source)
-				.then(res => res.text())
-				.then(res => { 
-					// Update content and content width
-					field.content = res;
-					field.width = field.content.length * 5.7;
-				})
-				.catch(ex => console.warn(ex));
-		}
+	const aTitle = accessibleTitle.join(" ");
+	const mappedStyle = mapStyle(badge.style);
+	return generateWrapper({
+		content: innerContent,
+		title: aTitle,
+		height: mappedStyle.height,
+		width: totalWidth,
+	});
+}
 
-		// Hex color string of partial
-		const colorString = mapColor(field.color);
-
-		const mappedStyle = mapStyle(style);
-		if (mappedStyle.textTransform)
-			field.content = mappedStyle.textTransform(field.content);
-		const width = Math.max(field.width * (mappedStyle.scale ?? 1) + 20, mappedStyle.height);
-		const opts: any = {
-			...mappedStyle,
-			color: colorString,
-			content: field.content,
-			iconURI: field.iconURI,
-			offset: offset,
-			width: width
-		};
-
-		return {
-			content: generateField(opts),
-			title: field.content,
-			width: width + (field.iconURI ? 10 : 0)
-		};
+async function generatePartial(field: BadgeField, style: BadgeStyle, offset = 0): Promise<BadgePartial> {
+	// Parse potential icon (:iconqualifier:)
+	if (field.iconURI) {
+		field.content = field.content.replace(/^:(.+):/, "").trim();
+		field.iconURI = await getIconDataURL(field.iconURI, true);
 	}
+
+	// If Dynamic, get the DVS content
+	if (field.source) {
+		await fetch(field.source)
+			.then(res => res.text())
+			.then(res => { 
+				// Update content and content width
+				field.content = res;
+				field.width = field.content.length * 5.7;
+			})
+			.catch(ex => console.warn(ex));
+	}
+
+	const mappedStyle = mapStyle(style);
+	if (mappedStyle.textTransform)
+		field.content = mappedStyle.textTransform(field.content);
+	const width = Math.max(field.width * (mappedStyle.scale ?? 1) + 20, mappedStyle.height);
+	const opts: any = {
+		...mappedStyle,
+		color: mapColor(field.color),
+		content: field.content,
+		iconURI: field.iconURI,
+		offset: offset,
+		width: width
+	};
+
+	return {
+		content: generateField(opts),
+		title: field.content,
+		width: width + (field.iconURI ? 10 : 0)
+	};
 }
--- icon/icon.ts
+++ icon/icon.ts
@@ -1,23 +1,19 @@
 
 import { exists } from "../deps.ts";
 
-export class IconService {
-	constructor() {}
+// Returns an SVG string or empty string
+// Gets icons from simpleicons
+export async function getIconDataURL(name: string, forceLight: boolean): Promise<string> {
+	const path = `icon/simpleicons/${name}.svg`;
+	if (await exists(path)) {
+		const decoder = new TextDecoder("utf-8");
+		let svgString = decoder.decode(await Deno.readFile(path));
 
-	// Returns an SVG string or empty string
-	// Gets icons from simpleicons
-	async getIconDataURL(name: string, forceLight: boolean): Promise<string> {
-		const path = `icon/simpleicons/${name}.svg`;
-		if (await exists(path)) {
-			const decoder = new TextDecoder("utf-8");
-			let svgString = decoder.decode(await Deno.readFile(path));
+		// Force fill white
+		if (forceLight)
+			svgString = svgString.replace("<path", "<path fill='#fff'");
 
-			// Force fill white
-			if (forceLight)
-				svgString = svgString.replace("<path", "<path fill='#fff'");
-
-			return "data:image/svg+xml;base64," + btoa(svgString);
-		}
-		else return "";
+		return "data:image/svg+xml;base64," + btoa(svgString);
 	}
+	else return "";
 }
