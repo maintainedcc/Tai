9ca0e2a32f3117ee711369c6fa03f6f474facb35
SDBagel <sdbagel@gmail.com>
2022-02-12 13:43:28 -0800

    repo: Remove unused deps

--- deps.ts
+++ deps.ts
@@ -1,11 +1,4 @@
 
-// Oak (Server)
-export { Application, Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
-
-// crypto & jose (JWT)
-export { crypto } from "https://deno.land/std@0.122.0/crypto/mod.ts";
-export { jwtVerify } from "https://deno.land/x/jose@v4.3.8/jwt/verify.ts";
-export { SignJWT } from "https://deno.land/x/jose@v4.3.8/jwt/sign.ts";
-
-// Environment
+// for badging server only
 import "https://deno.land/x/dotenv@v3.1.0/load.ts";
+export { Application, Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
