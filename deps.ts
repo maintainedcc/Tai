d20f222e890c8e4ef4925d5df673086481512cdc
SDBagel <sdbagel@gmail.com>
2022-02-11 23:24:01 -0800

    refactor: Remove exists dependency

--- deps.ts
+++ deps.ts
@@ -2,9 +2,6 @@
 // Oak (Server)
 export { Application, Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
 
-// std
-export { exists } from "https://deno.land/std@0.125.0/fs/exists.ts";
-
 // crypto & jose (JWT)
 export { crypto } from "https://deno.land/std@0.122.0/crypto/mod.ts";
 export { jwtVerify } from "https://deno.land/x/jose@v4.3.8/jwt/verify.ts";
--- icon/icon.ts
+++ icon/icon.ts
@@ -1,19 +1,13 @@
 
-import { exists } from "../deps.ts";
-
 // Returns an SVG string or empty string
 // Gets icons from simpleicons
 export async function getIconDataURL(name: string, forceLight: boolean): Promise<string> {
 	const path = `icon/simpleicons/${name}.svg`;
-	if (await exists(path)) {
-		const decoder = new TextDecoder("utf-8");
-		let svgString = decoder.decode(await Deno.readFile(path));
+	let svg = await Deno.readTextFile(path).catch(() => "");
+	if (!svg) return "";
 
-		// Force fill white
-		if (forceLight)
-			svgString = svgString.replace("<path", "<path fill='#fff'");
+	// Force fill white
+	if (forceLight) svg = svg.replace("<path", "<path fill='#fff'");
 
-		return "data:image/svg+xml;base64," + btoa(svgString);
-	}
-	else return "";
+	return "data:image/svg+xml;base64," + btoa(svg);
 }
