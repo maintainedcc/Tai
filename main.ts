3416bd5f648c36795753b7f1917d6326e3e06646
SDBagel <sdbagel@gmail.com>
2022-02-22 13:24:59 -0800

    feat: Add standalone badging without Maintained

--- main.ts
+++ main.ts
@@ -6,6 +6,25 @@ const app = new Application();
 const router = new Router();
 
 router
+	.get("/", async ctx => {
+		const defaultQuery = '{"fields":[{"content":"Tai","color":0,"width":20},{"content":"Invalid Badge","color":6,"width":80}],"style":0}';
+		const query = ctx.request.url.searchParams.get("json") || defaultQuery;
+			
+		// Set response headers
+		ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+		ctx.response.headers.set("Cache-Control", "no-store");
+		ctx.response.type = "image/svg+xml";
+
+		// Generate badge or invalid badge and return
+		try {
+			const json = JSON.parse(query);
+			ctx.response.body = await generate(json);
+		}
+		catch (e) {
+			const json = JSON.parse(defaultQuery);
+			ctx.response.body = await generate(json);
+		}
+	})
 	.get("/:userId/:project/:badgeId", async ctx => {
 		const { userId, project, badgeId } = ctx.params;
 		const apiBase = Deno.env.get("API_BASE") || "http://localhost:8002";
