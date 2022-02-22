9d497a45000d6c9fd72f0e9f05d48ba4d1824d29
SDBagel <sdbagel@gmail.com>
2022-02-22 13:00:41 -0800

    feat: Try query all available iconsets

--- icon/icon.ts
+++ icon/icon.ts
@@ -1,13 +1,33 @@
 
-// Returns an SVG string or empty string
-// Gets icons from simpleicons
-export async function getIconDataURL(name: string, forceLight: boolean): Promise<string> {
-	const path = `icon/simple-icons/icons/${name}.svg`;
-	let svg = await Deno.readTextFile(path).catch(() => "");
-	if (!svg) return "";
+// Icon set paths to query
+const iconSets = ["simple-icons/icons", "twbs-icons/icons"];
+// Reject reads if permission not granted
+const granted = (await Deno.permissions.query({ name: "read" })).state === "granted";
 
-	// Force fill white
+// Returns an SVG string or empty string if not found
+export async function getIconDataURL(name: string, forceLight: boolean): Promise<string> {
+	if (!granted) {
+		console.error("Permission denied: read access to icon files");
+		return "";
+	}
+
+	// Get SVG and change color to be white
+	let svg = await readSVG(name);
+	if (!svg) return "";
 	if (forceLight) svg = svg.replace("<path", "<path fill='#fff'");
 
+	// Return as data URL
 	return "data:image/svg+xml;base64," + btoa(svg);
+}
+
+// Checks all downloaded iconsets for icon presence
+async function readSVG(name: string): Promise<string> {
+	for (const iconSet of iconSets) {
+		const path = `icon/${iconSet}/${name}.svg`;
+		let svg = "";
+		try { svg = await Deno.readTextFile(path); }
+		catch (e) { continue; }
+		if (svg) return svg;
+	}
+	return "";
 }
